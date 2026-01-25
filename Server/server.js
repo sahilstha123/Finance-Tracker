20d374a6cfafedf56a29117a6a8e077ca87d4e35
Sahil shrestha <sthasahil401@gmail.com>
2026-01-25 18:05:10 +0545

    creating server

---
+++ Server/server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Server is running yaya ")
+})
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000")
+})
