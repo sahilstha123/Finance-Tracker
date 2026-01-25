18bc20c576427459a88e3db91e4701c47ac1746d
Sahil shrestha <sthasahil401@gmail.com>
2026-01-25 16:58:25 +0545

    added pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,22 +1,16 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import Button from "react-bootstrap/Button"
-import { ToastContainer, toast } from 'react-toastify';
+import Login from './pages/Login';
+import { Routes, Route } from "react-router-dom"
+import { ToastContainer, toast } from "react-toastify"
 
 function App() {
-  const notify = () => toast.success("Wow so easy!", {
-    position: "bottom-right",
-    autoClose: 3000,
-  });
+
   return (
     <>
-      Hi
-      <Button
-        variant="primary"
-        onClick={notify}
-      >
-        Primary
-      </Button>
+      <Routes>
+        <Route index element={<Login />} />
+      </Routes>
       <ToastContainer />
     </>
   )
