9e0bada07dd34ac13a7c241b77930cb68de63125
Sahil shrestha <sthasahil401@gmail.com>
2026-02-26 19:00:15 +0545

    working on transaction

--- Client/src/pages/Transaction.jsx
+++ Client/src/pages/Transaction.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 const Transaction = () => {
   return (
-    <div>Transaction</div>
+    <Container>
+      <Row>
+        <Col md={6}>
+        <div>Form here</div>
+        <hr />
+        <div>
+          table Here
+        </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
