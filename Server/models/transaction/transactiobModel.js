978d2d4bf5443cf50e6e042f72e9f51b733767ab
Sahil shrestha <sthasahil401@gmail.com>
2026-02-28 22:06:44 +0545

    feat: update transaction Modal Create logic

--- Server/models/transaction/transactiobModel.js
+++ Server/models/transaction/transactiobModel.js
@@ -1,5 +1,22 @@
 const transactionSchema = require("./transactionSchema")
 
-exports.insertNewTransaction = (transactionData)=>{
-    return transactionSchema(transactionData).save()
+exports.insertNewTransaction = async(transactionData)=>{
+
+    const {title, tdate, userId, type,amount} = transactionData
+
+    const existingTransaction = await transactionSchema.findOne({
+        userId,
+        title,
+        tdate,
+        type
+    })
+
+    if(existingTransaction)
+    {
+        existingTransaction.amount+= Number(amount)
+        return await existingTransaction.save()
+    }
+    else{
+        return await transactionSchema(transactionData).save()
+    }
 }
