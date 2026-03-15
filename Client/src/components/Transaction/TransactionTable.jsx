16762bb023d380dca943bef3bd3a3bfd5407c9ea
Sahil shrestha <sthasahil401@gmail.com>
2026-03-16 00:29:57 +0545

    WIP: add basic search and header in transactionTable

--- Client/src/components/Transaction/TransactionTable.jsx
+++ Client/src/components/Transaction/TransactionTable.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Table from 'react-bootstrap/Table'
+import {
+  FaSearch,
+  FaTrashAlt,
+  FaRegEdit,
+  FaFilter
+} from 'react-icons/fa'
+import "./TransactionTable.css"
 
 const TransactionTable = () => {
+  const [filter, setFIlter] = useState("all")
   return (
-    <div>TransactionTable</div>
-  )
+    <div className="transaction-table-container">
+      {/* title,search and filter */}
+      <div className="table-top-bar">
+        <h3>Transactions</h3>
+        <div className="search-box">
+          <FaSearch className='search-icon' />
+          <input
+            type="text"
+            placeholder='Search transactions....'
+            className='search-input' />
+
+        </div>
+      </div>
+    </div>
+
+  );
 }
 
 export default TransactionTable
