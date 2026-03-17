11663fc526bdf613c4a1f87a77022ef6443a1485
Sahil shrestha <sthasahil401@gmail.com>
2026-03-17 15:49:39 +0545

    feat:add transaction table UI

--- Client/src/components/Transaction/TransactionTable.jsx
+++ Client/src/components/Transaction/TransactionTable.jsx
@@ -4,12 +4,16 @@ import {
   FaSearch,
   FaTrashAlt,
   FaRegEdit,
-  FaFilter
+  FaFilter,
+  FaArrowAltCircleUp,
+  FaLongArrowAltDown,
+  FaArrowAltCircleDown
 } from 'react-icons/fa'
 import "./TransactionTable.css"
 
 const TransactionTable = () => {
-  const [filter, setFIlter] = useState("all")
+  const [filter, setFilter] = useState("all")
+  const buttons = ["All", "Credit", "debit"]
   return (
     <div className="transaction-table-container">
       {/* title,search and filter */}
@@ -21,8 +25,62 @@ const TransactionTable = () => {
             type="text"
             placeholder='Search transactions....'
             className='search-input' />
-
         </div>
+
+        <div className="filter-group">
+          {
+            buttons.map((item) => (
+              <button
+                key={item}
+                className={`btn-filter ${filter === item ? "active" : ""}`}
+                onClick={() => setFilter(item)}
+              >
+                {item}
+              </button>
+            ))
+          }
+        </div>
+      </div>
+
+      {/* Table Section */}
+      <div className="table-responsive">
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>S.N</th>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Credit</th>
+              <th>Debit</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>01</td>
+              <td><span className="date-text">2025-3-01</span></td>
+              <td className="fw-bold text-dark">Food</td>
+              <td>
+                <span className="transaction-badge badge-credit">
+                  <FaArrowAltCircleUp />
+                  <span className="amount-text">3500</span>
+                </span>
+              </td>
+              <td>
+                <span className="transaction-badge badge-debit">
+                  <FaArrowAltCircleDown />
+                  <span className="amount-text">3500</span>
+                </span>
+              </td>
+              <td>
+                <div className="action-btns">
+                  <div className="btn-action" title='Edit'><FaRegEdit /></div>
+                  <div className="btn-action btn-delete" title='Delete'><FaTrashAlt /></div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
 
